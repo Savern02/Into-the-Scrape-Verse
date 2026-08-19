50bbe4bdf0bcd6ad953521b73613f6ad8d4faf45
Savern02 <yoshishi.58@gmail.com>
2026-08-19 09:47:24 -0500

    cleaned up from default template

---
+++ web-app/components/main/hero.tsx
@@ -0,0 +1,10 @@
+export function Hero() {
+  return (
+    <div className="flex flex-col gap-16 items-center">
+      <div className="flex gap-8 justify-center items-center">
+        <span className="border-l rotate-45 h-6" />
+      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+    </div>
+    </div>
+  );
+}
