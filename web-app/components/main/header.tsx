01cab64d5cf7422cac449dda5e41ad2b6d958d9f
Savern02 <yoshishi.58@gmail.com>
2026-08-19 16:29:37 -0500

    updated and added images

--- web-app/components/main/header.tsx
+++ web-app/components/main/header.tsx
@@ -1,12 +1,27 @@
 import Link from "next/link";
+import Image from "next/image";
+import { AuthButton } from "../auth/auth-button.tsx"
+import { Suspense } from 'react';
+
 export function Header() {
   return (
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>KadooOsh</Link>
+              <Image
+                src="/logos/logo.png"
+                alt="Cheap Chick"
+                width={50}
+                height={50}
+              />
+              <Link href={"/"}>Cheap Chick</Link>
               <div className="flex items-center gap-2">
               </div>
+            </div>
+            <div>
+           <Suspense fallback={<div>Loading...</div>}>
+             <AuthButton />
+            </Suspense>
             </div>
           </div>
         </nav>
