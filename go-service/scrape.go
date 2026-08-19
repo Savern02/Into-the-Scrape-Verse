491a184093136b013ee607554a79ffcf625a87af
JohnGallemore <gallemorejohn5@gmail.com>
2026-08-18 22:53:31 -0500

    Began working on Go Services
    Got a very basic implementation of the API call and json response set up in the scraping go service.

---
+++ go-service/scrape.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type WalmartProductData struct {
+}
+
+type WalmartSearchPageData struct {
+}
+
+// func ScrapeWalmartProductData(productString string) error {
+func WalmartScrape() error {
+	var apiKey = os.Getenv("BRIGHT_DATA_API_KEY")
+	if apiKey == "" {
+		panic("BRIGHT_DATA_API_KEY environment variable is not set")
+	}
+
+	datasetId := "gd_l95fol7l1ru6rlo116"
+	endpoint := "https://api.brightdata.com/datasets/v3/scrape?dataset_id=" + datasetId + "&format=json"
+	payload := strings.NewReader(`[
+    {"url": "https://www.walmart.com/ip/Hershey-s-Kit-Kat-And-Reese-s-Assorted-Flavored-Candy-Party-Pack-33-38-oz/466623588"}
+	]`)
+
+	req, err := http.NewRequest("POST", endpoint, payload)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+apiKey)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Savern02/Into-the-Scrape-Verse/tree/main/go-service
+
+go 1.25.1
